279fc9782c860923c0377ce0e2a8808698e3bb86
lee8442 <014310@naver.com>
2020-10-06 22:04:23 +0900

    vue test

---
+++ vue-nodebird/ch1/front/components/FollowList.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-list>
+    <v-list-item>
+      <span>테스트</span>
+      <v-icon>mdi-minus-circle-outline</v-icon>
+    </v-list-item>
+    <v-list-item>
+      <span>테스트2</span>
+      <v-icon>mdi-minus-circle-outline</v-icon>
+    </v-list-item>
+    <v-list-item>
+      <span>테스트3</span>
+      <v-icon>mdi-minus-circle-outline</v-icon>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
